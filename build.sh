fb843e64964521d96ab4a19595b67542ee291cae
dungviettran89 <dungviettran89@gmail.com>
2019-12-02 13:00:45 +0000

    Release and tag version 0.0.3

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 set -e
 export MAIN=$(pwd)
 export VERSION=0.0.3
-git tag -a ${VERSION} -m "Released ${VERSION}"
 cd ${MAIN}/images//vnc-in-a-bottle
 docker build -t ide-in-a-bottle:${VERSION} .
 docker tag ide-in-a-bottle:${VERSION} ide-in-a-bottle:latest
 cd ${MAIN}/images/jdk-in-a-bottle
-docker build -t jdk-in-a-bottle:${VERSION} .
+docker build -t ide-in-a-bottle:${VERSION}-jdk .
 docker tag ide-in-a-bottle:${VERSION}-jdk ide-in-a-bottle:latest-jdk
 mkdir -p ${MAIN}/images/build
 docker save ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}.tar.gz
 docker save ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+git tag -a ${VERSION} -m "Released ${VERSION}"
+git push
