615c677453fe65525caf6a079daf8da15ef341e3
dungviettran89 <dungviettran89@gmail.com>
2019-12-08 15:27:01 +0000

    better startup script

--- images/ubuntu-jdk/entrypoint.sh
+++ images/ubuntu-jdk/entrypoint.sh
@@ -13,5 +13,5 @@ export DISPLAY=:99
 sleep 2s; fluxbox &
 sleep 2s; /usr/bin/x11vnc -rfbport "${VNC_PORT}" -passwd "${VNC_PASSWORD}" -bg -xkb -noxrecord -noxfixes -noxdamage -display :99  -nevershared -forever > /tmp/x11vnc.log &
 sleep 2s; /app/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT} > /tmp/novnc_server.log &
-sleep 2s; if [[ -f "/home/developer/startup.sh" ]]; then  bash /home/developer/startup.sh; else echo "You can auto start application by adding a new script at ~/startup.sh"; fi &
+sleep 2s; if [[ -f "/home/developer/startup.sh" ]]; then  chmod +x /home/developer/startup.sh; /home/developer/startup.sh; else echo "You can auto start application by adding a new script at ~/startup.sh"; fi &
 bash
