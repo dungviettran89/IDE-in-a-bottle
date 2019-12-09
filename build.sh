3ab254eac8af080db54be68cc9f3498dd2f31353
dungviettran89 <dungviettran89@gmail.com>
2019-12-09 07:14:27 +0000

    Expose ports

--- build.sh
+++ build.sh
@@ -8,17 +8,20 @@ mkdir -p ${MAIN}/build
 cd ${MAIN}/images/vnc-in-a-bottle
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION} .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION} radiocuatoi/ide-in-a-bottle:latest
-docker save radiocuatoi/ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}.tar.gz
+docker save radiocuatoi/ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}.tar.gz
+docker push radiocuatoi/ide-in-a-bottle:${VERSION}
 
 cd ${MAIN}/images/jdk-in-a-bottle
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-jdk .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-jdk radiocuatoi/ide-in-a-bottle:latest-jdk
-docker save radiocuatoi/ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+docker save radiocuatoi/ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+docker push radiocuatoi/ide-in-a-bottle:${VERSION}-jdk
 
 cd ${MAIN}/images/ubuntu-jdk
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk radiocuatoi/ide-in-a-bottle:latest-ubuntu-jdk
-docker save radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}-ubuntu-jdk.tar.gz
+docker save radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-ubuntu-jdk.tar.gz
+docker push radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk
 
 git tag -a ${VERSION} -m "Released ${VERSION}"
 git push --tags
