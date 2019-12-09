7fad5f0bc3c7a7128464fbc55e336fbb7f34c5a4
dungviettran89 <dungviettran89@gmail.com>
2019-12-09 02:34:14 +0000

    Publish images on dockerhub

--- build.sh
+++ build.sh
@@ -2,14 +2,23 @@
 set -e
 export MAIN=$(pwd)
 export VERSION=${VERSION:-0,0.1}
-cd ${MAIN}/images//vnc-in-a-bottle
-docker build -t ide-in-a-bottle:${VERSION} .
-docker tag ide-in-a-bottle:${VERSION} ide-in-a-bottle:latest
+
+mkdir -p ${MAIN}/build
+
+cd ${MAIN}/images/vnc-in-a-bottle
+docker build -t radiocuatoi/ide-in-a-bottle:${VERSION} .
+docker tag r:${VERSION} radiocuatoi/ide-in-a-bottle:latest
+docker save radiocuatoi/ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}.tar.gz
+
 cd ${MAIN}/images/jdk-in-a-bottle
-docker build -t ide-in-a-bottle:${VERSION}-jdk .
-docker tag ide-in-a-bottle:${VERSION}-jdk ide-in-a-bottle:latest-jdk
-mkdir -p ${MAIN}/images/build
-docker save ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}.tar.gz
-docker save ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-jdk .
+docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-jdk radiocuatoi/ide-in-a-bottle:latest-jdk
+docker save radiocuatoi/ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+
+cd ${MAIN}/images/ubuntu-jdk
+docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk .
+docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk radiocuatoi/ide-in-a-bottle:latest-ubuntu-jdk
+docker save radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk | gzip > ${MAIN}/build/radiocuatoi/ide-in-a-bottle-${VERSION}-ubuntu-jdk.tar.gz
+
 git tag -a ${VERSION} -m "Released ${VERSION}"
-git push
+git push --tags
