c8984f3421d3ce91af6d7dff8fcbc2d36a24baf0
dungviettran89 <dungviettran89@gmail.com>
2019-12-09 03:36:25 +0000

    Fixed problem with installing ping

--- images/ubuntu-jdk/dev.sh
+++ images/ubuntu-jdk/dev.sh
@@ -3,7 +3,7 @@ set -e
 . ./build.sh
 export CONTAINER=ide-in-a-bottle-ubuntu-jdk-latest
 docker rm -f "$CONTAINER" || true
-docker run --rm -it \
+docker run --rm -t --sig-proxy=true \
  --name="$CONTAINER" \
  -p 15900:5900 \
  "$IMAGE"
