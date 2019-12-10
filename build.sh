3b09ad342db985796ef95168f9ad4bfe501541c1
dungviettran89 <dungviettran89@gmail.com>
2019-12-10 03:23:44 +0000

    Minor fix with fluxbox

--- build.sh
+++ build.sh
@@ -8,18 +8,22 @@ mkdir -p ${MAIN}/build
 cd ${MAIN}/images/base
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION} .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION} radiocuatoi/ide-in-a-bottle:latest
-docker save radiocuatoi/ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}.tar.gz
-docker push radiocuatoi/ide-in-a-bottle:${VERSION}
 
 cd ${MAIN}/images/jdk
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-jdk .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-jdk radiocuatoi/ide-in-a-bottle:latest-jdk
-docker save radiocuatoi/ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-jdk.tar.gz
-docker push radiocuatoi/ide-in-a-bottle:${VERSION}-jdk
 
 cd ${MAIN}/images/ubuntu-jdk
 docker build -t radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk .
 docker tag radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk radiocuatoi/ide-in-a-bottle:latest-ubuntu-jdk
+
+echo "Exporting radiocuatoi/ide-in-a-bottle:${VERSION}"
+docker save radiocuatoi/ide-in-a-bottle:${VERSION} | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}.tar.gz
+docker push radiocuatoi/ide-in-a-bottle:${VERSION}
+echo "Exporting radiocuatoi/ide-in-a-bottle:${VERSION}-jdk"
+docker save radiocuatoi/ide-in-a-bottle:${VERSION}-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-jdk.tar.gz
+docker push radiocuatoi/ide-in-a-bottle:${VERSION}-jdk
+echo "Exporting radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk"
 docker save radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk | gzip > ${MAIN}/build/ide-in-a-bottle-${VERSION}-ubuntu-jdk.tar.gz
 docker push radiocuatoi/ide-in-a-bottle:${VERSION}-ubuntu-jdk
 
--- images/base/entrypoint.sh
+++ images/base/entrypoint.sh
@@ -13,7 +13,7 @@ echo "VNC_PORT=${VNC_PORT}"
 echo "VNC_PASSWORD=${VNC_PASSWORD}"
 echo "LANG=${LANG}"
 /usr/bin/Xvfb :99 -screen 0 "${WIDTH}x${HEIGHT}x24" > /tmp/xvfb.log &
-sleep 2s; startfluxbox &
+sleep 2s; fluxbox &
 sleep 2s; /usr/bin/x11vnc -rfbport "${VNC_PORT}" -passwd "${VNC_PASSWORD}" -bg -xkb -noxrecord -noxfixes -noxdamage -display :99  -nevershared -forever > /tmp/x11vnc.log &
 sleep 2s; /app/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT} > /tmp/novnc_server.log &
 sleep 2s; if [[ -f "/home/developer/startup.sh" ]]; then  chmod +x /home/developer/startup.sh; /home/developer/startup.sh; else echo "You can auto start application by adding a new script at ~/startup.sh"; fi &
