712f1903e721e03c86a6a5efeddc9718d3b068dd
dungviettran89 <dungviettran89@gmail.com>
2019-12-11 02:55:34 +0000

    Auto restart x11vnc, better support for remote control

--- images/base/entrypoint.sh
+++ images/base/entrypoint.sh
@@ -12,9 +12,22 @@ echo "NOVNC_PORT=${NOVNC_PORT}"
 echo "VNC_PORT=${VNC_PORT}"
 echo "VNC_PASSWORD=${VNC_PASSWORD}"
 echo "LANG=${LANG}"
-/usr/bin/Xvfb :99 -screen 0 "${WIDTH}x${HEIGHT}x24" > /tmp/xvfb.log &
-sleep 2s; fluxbox &
-sleep 2s; /usr/bin/x11vnc -rfbport "${VNC_PORT}" -passwd "${VNC_PASSWORD}" -bg -xkb -noxrecord -noxfixes -noxdamage -display :99  -nevershared -forever > /tmp/x11vnc.log &
-sleep 2s; /app/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT} > /tmp/novnc_server.log &
-sleep 2s; if [[ -f "/home/developer/startup.sh" ]]; then  chmod +x /home/developer/startup.sh; /home/developer/startup.sh; else echo "You can auto start application by adding a new script at ~/startup.sh"; fi &
+
+keepAlive() {
+    while true; do
+        $@;
+        sleep 10s;
+    done
+}
+
+/usr/bin/Xvfb :99 -screen 0 "${WIDTH}x${HEIGHT}x24" &
+sleep 2s; keepAlive fluxbox &
+sleep 2s; keepAlive /usr/bin/x11vnc -rfbport "${VNC_PORT}" -passwd "${VNC_PASSWORD}" -xkb -noxrecord -noxfixes -noxdamage -display :99 -nevershared -forever &
+sleep 2s; keepAlive /app/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT} &
+sleep 10s;
+if [[ -f "/home/developer/startup.sh" ]]; then
+    chmod +x /home/developer/startup.sh; /home/developer/startup.sh;
+else
+    echo "You can auto start application by adding a new script at ~/startup.sh";
+fi &
 bash
