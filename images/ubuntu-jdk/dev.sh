64849ead2a8dbc0966b70b0ff268398c9237218a
dungviettran89 <dungviettran89@gmail.com>
2020-03-16 16:36:07 +0800

    Move ubuntu image to use tiger vnc

--- images/ubuntu-jdk/dev.sh
+++ images/ubuntu-jdk/dev.sh
@@ -5,5 +5,5 @@ export CONTAINER=ide-in-a-bottle-ubuntu-jdk-latest
 docker rm -f "$CONTAINER" || true
 docker run --rm -it \
  --name="$CONTAINER" \
- -p 15900:5900 \
+ -p 16080:6080 \
  "$IMAGE"
--- images/ubuntu-jdk/entrypoint.sh
+++ images/ubuntu-jdk/entrypoint.sh
@@ -2,7 +2,7 @@
 export WIDTH=${WIDTH:-1400}
 export HEIGHT=${HEIGHT:-700}
 export NOVNC_PORT=${NOVNC_PORT:-6080}
-export VNC_PORT=${VNC_PORT:-5900}
+export VNC_PORT=${VNC_PORT:-5999}
 export VNC_PASSWORD=${VNC_PASSWORD:-demo}
 export LANG=${LANG:-en_GB.UTF-8}
 export DISPLAY=:99
@@ -19,10 +19,21 @@ keepAlive() {
         sleep 10s;
     done
 }
+mkdir -p ~/.vnc/
+echo "${VNC_PASSWORD}" | /usr/bin/tigervncpasswd -f > ~/.vnc/passwd &
 
-/usr/bin/Xvfb :99 -screen 0 "${WIDTH}x${HEIGHT}x24" &
+mkdir -p ~/.fluxbox/
+cat <<EOT > ~/.fluxbox/menu
+[begin] (fluxbox)
+[exec] (Chrome) {/usr/bin/chromium-browser --no-gpu --no-sandbox --disable-dev-shm-usage --disable-web-security} <>
+[exec] (Intellij IDEA) {/app/idea*/bin/idea.sh} <>
+[end]
+EOT
+
+
+echo "Starting development environment"
+/usr/bin/tigervncserver :99 -httpPort ${VNC_PORT} -passwd ~/.vnc/passwd -geometry "${WIDTH}x${HEIGHT}" &
 sleep 2s; keepAlive fluxbox &
-sleep 2s; keepAlive /usr/bin/x11vnc -rfbport "${VNC_PORT}" -passwd "${VNC_PASSWORD}" -xkb -noxrecord -noxfixes -noxdamage -display :99 -nevershared -forever &
 sleep 2s; keepAlive /app/novnc/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT} &
 sleep 10s;
 if [[ -f "/home/developer/startup.sh" ]]; then
